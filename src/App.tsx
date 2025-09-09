bf4a0d1f1aad18a27eae1d2bffeb58c4181a92d5
Musfir <musfirali1@gmail.com>
2025-09-09 23:44:33 +0500

    Initial Committ

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Hello Brother</h1>
+    <p className='mt-4'>My name is Musfir & I'm a full stack developer</p>
+    </>
+  )
+}
+
+export default App
