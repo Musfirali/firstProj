e3406e4035ed280b0852c127805f294f03244580
Musfir <musfirali1@gmail.com>
2025-09-10 03:50:53 +0500

    Played with Buttons learning React Components

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ function App() {
     <h1>Hello Brother</h1>
     <p className='mt-4'>This is Musfir & I'm a full stack developer</p>
     <Button/>
+    <Button text='Book a call now'/>
+    <Button text='Sign up'></Button>
     </>
   )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,8 @@
-function Button({text = "submit"}){
-    return <button>{text}</button>
+function Button({text}){
+    if(!text){
+        text = "XaXaXa"
+    }
+    return <button className="mt-3">{text}</button>
 }
 
 export default Button
