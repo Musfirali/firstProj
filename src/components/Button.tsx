010fd1cf776395bbf26e9796ea36c7723573125b
Musfir Ali <musfirali1@gmail.com>
2025-10-12 16:14:00 +0500

    Event Handlers -- 2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
-function Button({text}){
-    if(!text){
-        text = "XaXaXa"
-    }
-    return <button className="mt-3">{text}</button>
-}
+// function Button (text: string)=>{
+//     if(!text){
+//         text = "XaXaXa"
+//     }
+//     return <button className="mt-3">{text}</button>
+// }
 
-export default Button
+// export default Button
--- src/components/LoginClass.tsx
+++ src/components/LoginClass.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, type SyntheticEvent } from "react"
+
 
 
 class LoginClass extends React.Component{
